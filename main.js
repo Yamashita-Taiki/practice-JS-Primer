32f43c6d3bae37ca92223126cbe12409daf43982
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-20 14:13:11 +0900

    switch学習

--- main.js
+++ main.js
@@ -4,31 +4,20 @@
 // ただし、西暦で示した年が100で割り切れる年はうるう年ではありません
 // ただし、西暦で示した年が400で割り切れる年はうるう年です
 
-const year = new Date().getFullYear();
+// const year = new Date().getFullYear();  ←動かない
+const year = "2021"　　　　　　　　　　　　　
+// ←動く
 
 switch (year) {
-  case "2019":
-    console.log('2019');
-    break;
-  case '2020':
-    console.log('2020');
-    break;
-   case '2021':
-    console.log('2021');
-    break;
-  default: 
-   console.log('unknown');
-   break;
+    case "2020":
+        console.log("2020");
+        break;
+    case "2021": 
+        console.log("2021");
+        break;
+    case "2022": 
+        console.log("2022");
+        break;
+    default:    
+        console.log("しらない");
 }
-
-// const version = "ES6";
-// switch (version) {
-//     case "ES5":
-//         console.log("ECMAScript 5");
-//     case "ES6": // 一致するケース
-//         console.log("ECMAScript 2015");
-//     case "ES7": // breakされないため条件無視して実行
-//         console.log("ECMAScript 2016");
-//     default:    // breakされないため条件無視して実行
-//         console.log("しらないバージョンです");
-// }
