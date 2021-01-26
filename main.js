2647ce8428816240513ae2a5e2643d93c96bd2cb
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-26 22:16:21 +0900

    配列

--- main.js
+++ main.js
@@ -4,18 +4,23 @@
 // ただし、西暦で示した年が100で割り切れる年はうるう年ではありません
 // ただし、西暦で示した年が400で割り切れる年はうるう年です
 
-// const year = new Date().getFullYear();  ←動かない
-const year = "2021"　　　　　　　　　　　　　
+const year = new Date().getFullYear(); 
+// const year = "2021"　　　　　　　　　　　　　
 // ←動く
 
+console.log(year);
+console.log("2021");
+console.log(2021);
+
+
 switch (year) {
-    case "2020":
+    case 2020:
         console.log("2020");
         break;
-    case "2021": 
+    case 2021: 
         console.log("2021");
         break;
-    case "2022": 
+    case 2022: 
         console.log("2022");
         break;
     default:    
@@ -101,21 +106,21 @@ console.log(en); // => "英語"
 
 // 未定義の箇所が1つ含まれる疎な配列
 // インデックスが1の値を省略しているので、カンマが2つ続いていることに注意
-const sparseArray = [1,, 3];
-console.log(sparseArray.length); // => 3
+// const sparseArray = [1,, 3];
+// console.log(sparseArray.length); // => 3
 // 1番目の要素は存在しないため undefined が返る
-console.log(sparseArray[1]); // => undefined
+// console.log(sparseArray[1]); // => undefined
 
 
 // 疎な配列で該当するインデックスに要素がない場合はundefinedを返します。 しかし、undefinedという値も存在するため、配列にundefinedという値がある場合に区別できません。
 // 次のコードでは、undefinedという値を要素として定義した密な配列と、要素そのものがない疎な配列を定義しています。 どちらも要素にアクセスした結果はundefinedとなり、区別できていないことがわかります。
 
 // 要素として`undefined`を持つ密な配列
-const denseArray = [1, undefined, 3];
-// 要素そのものがない疎な配列
-const sparseArray = [1, , 3];
-console.log(denseArray[1]); // => undefined
-console.log(sparseArray[1]); // => undefined
+// const denseArray = [1, undefined, 3];
+// // 要素そのものがない疎な配列
+// const sparseArray = [1, , 3];
+// console.log(denseArray[1]); // => undefined
+// console.log(sparseArray[1]); // => undefined
 
 
 // この違いを見つける方法として利用できるのがObject#hasOwnPropertyメソッドです。 hasOwnPropertyメソッドを使うことで、配列の指定したインデックスに要素自体が存在するかを判定できます。
@@ -131,9 +136,24 @@ console.log(sparseArray.hasOwnProperty(1)); // => false
 // 指定した要素が配列のどの位置にあるかを知りたい場合、Array#indexOfメソッドやArray#findIndexメソッド[ES2015]を利用します。 要素の位置のことをインデックス（index）と呼ぶため、メソッド名にもindexという名前が入っています。
 // 次のコードでは、Array#indexOfメソッドを利用して、配列の中から"JavaScript"という文字列のインデックスを取得しています。 indexOfメソッドは引数と厳密等価演算子（===）で一致する要素があるなら、その要素のインデックスを返し、該当する要素がない場合は-1を返します。 indexOfメソッドは先頭から検索して見つかった要素のインデックスを返します。 indexOfメソッドには対となるArray#lastIndexOfメソッドがあり、lastIndexOfメソッドでは末尾から検索した結果が得られます。
 
-const array = ["Java", "JavaScript", "Ruby"];
-const indexOfJS = array.indexOf("JavaScript");
-console.log(indexOfJS); // => 1
-console.log(array[indexOfJS]); // => "JavaScript"
-// "JS" という要素はないため `-1` が返される
-console.log(array.indexOf("JS")); // => -1
+// const array = ["Java", "JavaScript", "Ruby"];
+// const indexOfJS = array.indexOf("JavaScript");
+// console.log(indexOfJS); // => 1
+// console.log(array[indexOfJS]); // => "JavaScript"
+// // "JS" という要素はないため `-1` が返される
+// console.log(array.indexOf("JS")); // => -1
+
+
+// このように、異なるオブジェクトだが値は同じものを見つけたい場合には、Array#findIndexメソッドが利用できます。 findIndexメソッドの引数には配列の各要素をテストする関数をコールバック関数として渡します。 indexOfメソッドとは異なり、テストする処理を自由に書けます。 これにより、プロパティの値が同じ要素を配列から見つけて、その要素のインデックスを得ることができます。
+// colorプロパティを持つオブジェクトの配列
+const colors = [
+    { "color": "red" },
+    { "color": "green" },
+    { "color": "blue" }
+];
+// `color`プロパティが"blue"のオブジェクトのインデックスを取得
+const indexOfBlue = colors.findIndex((obj) => {
+    return obj.color === "red";
+});
+console.log(indexOfBlue); // => 2
+console.log(colors[indexOfBlue]); // => { "color": "blue" }
